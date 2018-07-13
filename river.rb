88cd0e465a61104eb0aad549f08eb7e90f74c77b
GeekyD1no <21achahn@bishopkelley.org>
2018-07-13 14:15:26 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,41 @@
 # river.rb
+ 
+ class River 
+  
+  def initialize(name)
+    @name = name 
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(name)
+    @name = name 
+  end 
+  
+  def length=(length) 
+    @length = length 
+  end
+  
+  def length
+    @length 
+  end
+  
+  def countries=(countries)
+    @countries = countries 
+  end
+  
+  def countries
+    @countries
+  end 
+  
+  def flood=(flood)
+    @flood 1.3
+  end
+  
+  def dry_up=(dry_up)
+    @dry_up .5
+end 
+
+end 
