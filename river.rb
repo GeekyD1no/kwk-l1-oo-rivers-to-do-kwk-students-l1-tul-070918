03700158911906a2d06bd9dd6a710b6fe8246cb1
GeekyD1no <21achahn@bishopkelley.org>
2018-07-13 14:25:42 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -31,11 +31,11 @@
   end 
   
  def flood
-    @discharge = discharge*1.3
+    @discharge= discharge*1.3
   end
   
   def dry_up
-    @discharge = discharge*0.5
+    @discharge= discharge*0.5
 end 
 
 end 
